c4aaa9d4f4c2a6d69eded64f4d23473405f95b23
Lidia <lidiayang104@gmail.com>
2023-08-11 19:56:50 +0200

    correccion de model proyecto

--- src/models/proyecto.model.js
+++ src/models/proyecto.model.js
@@ -28,15 +28,15 @@ const deleteProyecto = (proyectoId) => {
 
 const getMonth = (usuarioId, proyectoId, mes) => {
     return db.query(
-        'select up.horas_dedicadas, up.fecha, p.nombre from mydb.usuarios_has_proyectos as up, proyectos as p  where p.id=up.proyectos_id and Usuarios_id=? and proyectos_id=? and month(fecha)=? ', [usuarioId, proyectoId, mes]
+        'select up.horas_dedicadas, up.fecha, p.nombre from mydb.usuarios_has_proyectos as up, proyectos as p  where p.id=up.proyectos_id and usuarios_id=? and proyectos_id=? and month(fecha)=? ', [usuarioId, proyectoId, mes]
     )
 }
 
 const getAllHourByProyect = (mes, usuarioId) => {
-    return db.query('SELECT proyectos_id, SUM(horas_dedicadas) AS total_horas_dedicadas FROM usuarios_has_proyectos where month(fecha)=? and usuarios_id=? GROUP BY proyectos_id;', [mes, usuarioId])
+    return db.query('select proyectos_id, sum(horas_dedicadas) as total_horas_dedicadas from usuarios_has_proyectos where month(fecha)=? and usuarios_id=? group by proyectos_id;', [mes, usuarioId])
 }
 const getHorasExtras = (usuarioId, mes) => {
-    return db.query('SELECT  p.nombre AS nombre_proyecto, SUM(CASE WHEN horas_dedicadas > 8 THEN horas_dedicadas - 8 END) AS horas_extra_total  FROM usuarios_has_proyectos up JOIN proyectos p ON up.proyectos_id = p.id WHERE Usuarios_id = ? and month(fecha)=? GROUP BY p.nombre', [usuarioId, mes,])
+    return db.query('select p.nombre as nombre_proyecto, sum(case when horas_dedicadas > 8 then horas_dedicadas - 8 end) as horas_extra_total  from usuarios_has_proyectos up join proyectos p on up.proyectos_id = p.id where usuarios_id = ? and month(fecha)=? group by p.nombre', [usuarioId, mes,])
 }
 
 /* const getAllHour = (mes, usuarioId) => {
