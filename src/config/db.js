493c7fc197eb11643fd92b8ac8b643eee80016ad
Lidia <lidiayang104@gmail.com>
2023-08-11 17:54:14 +0200

    online db add

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,28 @@
 const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//     host: process.env.HOSTDB,
+//     user: process.env.USERDB,
+//     password: process.env.PASSDB,
+//     port: process.env.PORTDB,
+//     database: process.env.DBNAME
+// });
+
 const pool = mysql.createPool({
-    host: process.env.HOSTDB,
-    user: process.env.USERDB,
-    password: process.env.PASSDB,
-    port: process.env.PORTDB,
-    database: process.env.DBNAME
+    host: db4free.net,
+    user: lidia888,
+    password: dcec6ac3,
+    port: 3306,
+    database: dbdetimehub
 });
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to db4free:', err);
+      return;
+    }
+    console.log('Connected to db4free database!');
+  });
 
 
 
